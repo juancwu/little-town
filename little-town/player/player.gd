7aab8edac181789ebd231119cfc062bcde5916ee
jc <46619361+juancwu@users.noreply.github.com>
2024-10-24 01:03:43 -0400

    add basic player with input and animation

--- little-town/player/player.gd
+++ little-town/player/player.gd
@@ -2,7 +2,39 @@ extends CharacterBody2D
 
 
 @export var MAX_SPEED = 300;
-@export var ACCELERATIOn = 1500;
+@export var ACCELERATION = 1500;
 @export var FRICTION = 1200;
 
 @onready var axis = Vector2.ZERO
+@onready var anim = $AnimatedSprite2D
+
+func _ready() -> void:
+	anim.play("idle")
+
+func _physics_process(delta: float) -> void:
+	move(delta)
+	
+func get_input_axis():
+	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	return axis.normalized()
+
+func move(delta):
+	axis = get_input_axis()
+	if axis == Vector2.ZERO:
+		anim.play("idle")
+		apply_friction(FRICTION * delta)
+	else:
+		anim.play("move")
+		apply_movement(axis * ACCELERATION * delta)
+	move_and_slide()
+	
+func apply_friction(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+		
+func apply_movement(accel):
+	velocity += accel
+	velocity = velocity.limit_length(MAX_SPEED)
